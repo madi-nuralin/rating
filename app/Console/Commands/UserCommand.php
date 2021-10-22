b05e1f5583ace74fd10e377a2bc50e847dd5a508
Madi Nuralin <nmdbzk@gmail.com>
2021-10-22 17:13:31 +0600

    add user role artisan command

---
+++ app/Console/Commands/UserCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Models\User;
+use App\Models\Role;
+
+class UserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:user:role:add {role} {email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add role to user by email given';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return User::firstWhere('email', $this->argument('email'))->roles()->attach(Role::firstWhere('context', $this->argument('role'))->getId());
+    }
+}
