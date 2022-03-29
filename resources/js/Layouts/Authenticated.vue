7ad3ff3bdc1fa3268c51c331bb57a9b865f63bf1
Madi Nuralin <nmdbzk@gmail.com>
2022-03-29 19:53:42 +0600

    update feed

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -26,8 +26,8 @@
                                         <div>
                                             <svg 
                                                 xmlns="http://www.w3.org/2000/svg"
-                                                width="20"
-                                                height="20"
+                                                width="24"
+                                                height="24"
                                                 viewBox="0 0 24 24"
                                                 fill="none"
                                                 stroke="currentColor"
@@ -48,8 +48,8 @@
                                         <div>
                                             <svg 
                                                 xmlns="http://www.w3.org/2000/svg"
-                                                width="20"
-                                                height="20"
+                                                width="24"
+                                                height="24"
                                                 viewBox="0 0 24 24"
                                                 fill="none"
                                                 stroke="currentColor"
