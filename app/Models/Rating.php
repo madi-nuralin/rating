6d9a2ca12baa522c8ceea2b31623b61c5165aa5b
Madi Nuralin <nmdbzk@gmail.com>
2022-04-14 20:27:40 +0600

    fix time

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -82,20 +82,32 @@ class Rating extends Model
         )->format(Rating::MYSQL_DATE_FORMAT);
     }
 
-    public function getTime1() {
-    	return date(Rating::HTML_DATE_FORMAT, strtotime($this->time1));
+    public function getTime1($dateFormat=null) {
+        if (is_null($dateFormat)) {
+            return $this->time1;
+        }
+    	return date($dateFormat, strtotime($this->time1));
     }
 
-    public function getTime2() {
-    	return date(Rating::HTML_DATE_FORMAT, strtotime($this->time2));
+    public function getTime2($dateFormat=null) {
+        if (is_null($dateFormat)) {
+            return $this->time2;
+        }
+    	return date($dateFormat, strtotime($this->time2));
     }
 
-    public function getTime3() {
-    	return date(Rating::HTML_DATE_FORMAT, strtotime($this->time3));
+    public function getTime3($dateFormat=null) {
+        if (is_null($dateFormat)) {
+            return $this->time3;
+        }
+    	return date($dateFormat, strtotime($this->time3));
     }
 
-    public function getTime4() {
-    	return date(Rating::HTML_DATE_FORMAT, strtotime($this->time4));
+    public function getTime4($dateFormat=null) {
+        if (is_null($dateFormat)) {
+            return $this->time4;
+        }
+    	return date($dateFormat, strtotime($this->time4));
     }
 
     public function getUserScore(User $user, $verificationStatuses = null /*array of ids'*/) {
@@ -131,10 +143,10 @@ class Rating extends Model
     		'id' => $this->getId(),
     		'name' => $this->getName(),
     		'description' => $this->getDescription(),
-    		'time1' => $this->getTime1(),
-    		'time2' => $this->getTime2(),
-    		'time3' => $this->getTime3(),
-    		'time4' => $this->getTime4(),
+    		'time1' => $this->getTime1(Rating::HTML_DATE_FORMAT),
+    		'time2' => $this->getTime2(Rating::HTML_DATE_FORMAT),
+    		'time3' => $this->getTime3(Rating::HTML_DATE_FORMAT),
+    		'time4' => $this->getTime4(Rating::HTML_DATE_FORMAT),
     		'created_at' => $this->created_at,
     		'updated_at' => $this->updated_at
     	];
