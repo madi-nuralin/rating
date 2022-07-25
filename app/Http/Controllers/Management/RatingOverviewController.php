062cd52d1480f035b9ae16b0956526bf7aa71efc
dkhojayev <dkhojayev@iitu.edu.kz>
2022-07-25 15:09:19 +0600

    добавил фильтры на странице отсчетов рейтинга

--- app/Http/Controllers/Management/RatingOverviewController.php
+++ app/Http/Controllers/Management/RatingOverviewController.php
@@ -31,66 +31,88 @@ class RatingOverviewController extends Controller
     {
         $rating = Rating::findOrFail($rating);
 
+        $email = request()->input('email');
+
+        $isHaveSubmissions = request()->input('isHaveSubmissions');
+
+        if ($isHaveSubmissions == 'true') {
+            $isHaveSubmissions = true;
+        } else {
+            $isHaveSubmissions = false;
+        }
+
         return Inertia::render('Management/Rating/Partials/Overview/Index', [
-             'rating' => array_merge(
+            'rating' => array_merge(
                 $rating->toArray(), [
-                    'users' => $rating->users()->paginate(10)->through(function($user) use ($rating) {
-                        return array_merge(
-                            $user->toArray(), [
-                                'is_approved' => $rating->userIsApproved($user),
-                                'employements' => $user->employements ? $user->getEmployements(NULL, DB::raw('CURDATE()'))->map(function($employement) {
-                                    return array_merge(
-                                        $employement->toArray(), [
-                                            'department' => $employement->department->toArray(),
-                                            'position' => $employement->position->toArray(),
-                                        ]
-                                    );
-                                }) : array(),
-                                'verifiers' => $rating->verifiers->map(function($verifier) use ($user) {
-                                    return array_merge(
-                                        $verifier->toArray(), [
-                                            'user' => $verifier->user->toArray(),
-                                            'statistics' => [
-                                                'total' => count(
-                                                    collect(
-                                                        $verifier->verifications()
-                                                            ->whereHas('submission', function($q) use ($user) {
-                                                                $q->where('user_id', $user->id);
-                                                            })
-                                                        ->get()
-                                                    )
-                                                ),
-                                                'completed' => count(
-                                                    collect(
-                                                        $verifier->verifications()
-                                                            ->whereHas('submission', function($q) use ($user) {
-                                                                $q->where('user_id', $user->id);
-                                                            })
-                                                            ->whereHas('verificationStatus', function($q) {
-                                                                $q->where('context', 'accepted')
-                                                                    ->orWhere('context', 'fixed')
-                                                                    ->orWhere('context', 'not_accepted');
-                                                            })
-                                                        ->get()
-                                                    )
-                                                )
+                    'users' => $rating->users()
+                        ->when(isset($email), function ($q) use ($email) {
+                            $q->where('email', 'like', '%' . $email . '%');
+                        })
+                        ->when($isHaveSubmissions, function ($q) use ($rating) {
+                            $q->whereHas('submissions', function ($q) use ($rating) {
+                                $q->where('rating_id', $rating->id);
+                            });
+                        })
+                        ->paginate(10)->withQueryString()->through(function ($user) use ($rating) {
+                            return array_merge(
+                                $user->toArray(), [
+                                    'is_approved' => $rating->userIsApproved($user),
+                                    'employements' => $user->employements ? $user->getEmployements(NULL, DB::raw('CURDATE()'))->map(function ($employement) {
+                                        return array_merge(
+                                            $employement->toArray(), [
+                                                'department' => $employement->department->toArray(),
+                                                'position' => $employement->position->toArray(),
                                             ]
-                                        ]
-                                    );
-                                })
-                            ]
-                        );
-                    })
+                                        );
+                                    }) : array(),
+                                    'verifiers' => $rating->verifiers->map(function ($verifier) use ($user) {
+                                        return array_merge(
+                                            $verifier->toArray(), [
+                                                'user' => $verifier->user->toArray(),
+                                                'statistics' => [
+                                                    'total' => count(
+                                                        collect(
+                                                            $verifier->verifications()
+                                                                ->whereHas('submission', function ($q) use ($user) {
+                                                                    $q->where('user_id', $user->id);
+                                                                })
+                                                                ->get()
+                                                        )
+                                                    ),
+                                                    'completed' => count(
+                                                        collect(
+                                                            $verifier->verifications()
+                                                                ->whereHas('submission', function ($q) use ($user) {
+                                                                    $q->where('user_id', $user->id);
+                                                                })
+                                                                ->whereHas('verificationStatus', function ($q) {
+                                                                    $q->where('context', 'accepted')
+                                                                        ->orWhere('context', 'fixed')
+                                                                        ->orWhere('context', 'not_accepted');
+                                                                })
+                                                                ->get()
+                                                        )
+                                                    )
+                                                ]
+                                            ]
+                                        );
+                                    })
+                                ]
+                            );
+                        })
                 ]
             ),
             'statistics' => [
                 'total' => $rating ? count($rating->users) : 0,
                 'last' => 0///$approver ? count($approver->rating->users) : 0
-            ]
+            ],
+
+            'email' => $email,
+            'isHaveSubmissions' => $isHaveSubmissions,
         ]);
     }
 
-    public function downloadReport($rating, $user) 
+    public function downloadReport($rating, $user)
     {
         $rating = Rating::findOrFail($rating);
         $user = User::findOrFail($user);
@@ -110,65 +132,65 @@ class RatingOverviewController extends Controller
             array('name' => 'Times New Roman', 'size' => 14 /*'color' => '1B2232'*/, 'bold' => false)
         );
 
-        $phpWord->addParagraphStyle('p2Style', array('align'=>'right'));
+        $phpWord->addParagraphStyle('p2Style', array('align' => 'right'));
 
         $section = $phpWord->addSection();
-            $section->addText(
-                'Лист оценки ППС',
-                $fontStyleBold,
-                'p2Style'
-            );
-            $section->addTextBreak(1);
-            $section->addText(
-                'Утверждаю',
-                $fontStyleBold,
-                'p2Style'
-            );
-            $section->addText(
-                'Зав.кафедрой ___________',
-                $fontStyleNormal,
-                'p2Style'
-            );
-            $section->addText(
-                'ФИО, (подпись)',
-                $fontStyleNormal,
-                'p2Style'
-            );
-            $section->addText(
-                '«_______» _____________ 20__ г.',
-                $fontStyleNormal,
-                'p2Style'
-            );
-            $section->addTextBreak(1);
-            $tableStyle = new \PhpOffice\PhpWord\Style\Table;
-            $tableStyle->setBorderColor('000000');
-            $tableStyle->setBorderSize(1);
-            $tableStyle->setUnit(\PhpOffice\PhpWord\Style\Table::WIDTH_PERCENT);
-            $tableStyle->setWidth(100 * 50);
+        $section->addText(
+            'Лист оценки ППС',
+            $fontStyleBold,
+            'p2Style'
+        );
+        $section->addTextBreak(1);
+        $section->addText(
+            'Утверждаю',
+            $fontStyleBold,
+            'p2Style'
+        );
+        $section->addText(
+            'Зав.кафедрой ___________',
+            $fontStyleNormal,
+            'p2Style'
+        );
+        $section->addText(
+            'ФИО, (подпись)',
+            $fontStyleNormal,
+            'p2Style'
+        );
+        $section->addText(
+            '«_______» _____________ 20__ г.',
+            $fontStyleNormal,
+            'p2Style'
+        );
+        $section->addTextBreak(1);
+        $tableStyle = new \PhpOffice\PhpWord\Style\Table;
+        $tableStyle->setBorderColor('000000');
+        $tableStyle->setBorderSize(1);
+        $tableStyle->setUnit(\PhpOffice\PhpWord\Style\Table::WIDTH_PERCENT);
+        $tableStyle->setWidth(100 * 50);
 
-            $table = $section->addTable('table', $tableStyle);
-            $table->addRow();
-            $table->addCell(5000)->addText("ЛИСТ ОЦЕНКИ (ППС)", $fontStyleBold);
+        $table = $section->addTable('table', $tableStyle);
+        $table->addRow();
+        $table->addCell(5000)->addText("ЛИСТ ОЦЕНКИ (ППС)", $fontStyleBold);
 
-            $table->addRow();
-            $table->addCell(5000)->addText("Фамилия, имя, отчество", $fontStyleBold);
-            $table->addCell(5000)->addText($user->name, $fontStyleNormal);
+        $table->addRow();
+        $table->addCell(5000)->addText("Фамилия, имя, отчество", $fontStyleBold);
+        $table->addCell(5000)->addText($user->name, $fontStyleNormal);
 
-            $table->addRow();
-            $table->addCell(5000)->addText("Наименование кафедры", $fontStyleBold);
-            $table->addCell(5000)->addText(
-                $user->getEmployements(NULL, DB::raw('CURDATE()')) ? $user->getEmployements(NULL, DB::raw('CURDATE()'))->map(function($employement) {
-                    return $employement->department->getName();
-                }) : 'Информация об отделе не найдена', $fontStyleNormal
-            );
+        $table->addRow();
+        $table->addCell(5000)->addText("Наименование кафедры", $fontStyleBold);
+        $table->addCell(5000)->addText(
+            $user->getEmployements(NULL, DB::raw('CURDATE()')) ? $user->getEmployements(NULL, DB::raw('CURDATE()'))->map(function ($employement) {
+                return $employement->department->getName();
+            }) : 'Информация об отделе не найдена', $fontStyleNormal
+        );
 
-            $table->addRow();
-            $table->addCell(5000)->addText("Должность", $fontStyleBold);
-            $table->addCell(5000);
+        $table->addRow();
+        $table->addCell(5000)->addText("Должность", $fontStyleBold);
+        $table->addCell(5000);
 
-            $table->addRow();
-            $table->addCell(5000)->addText("Стаж работы в занимаемой должности", $fontStyleBold);
-            $table->addCell(5000);
+        $table->addRow();
+        $table->addCell(5000)->addText("Стаж работы в занимаемой должности", $fontStyleBold);
+        $table->addCell(5000);
 
 
         $phpWord->save('teste.docx', 'Word2007', true);
@@ -188,7 +210,7 @@ class RatingOverviewController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -199,7 +221,7 @@ class RatingOverviewController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -210,7 +232,7 @@ class RatingOverviewController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -221,8 +243,8 @@ class RatingOverviewController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -233,7 +255,7 @@ class RatingOverviewController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
